b711647dbc70f06d8207d257c7b4ccf2ec10dc0e
Richard Jones <wigsaparelli@gmail.com>
2021-05-21 12:48:02 +0100

    Update pingdom-check-heatmap.js

--- pingdom-check-heatmap.js
+++ pingdom-check-heatmap.js
@@ -2,9 +2,26 @@
             return fetcher('graph-custom', { gremlinQuery, bindings }, context, timeframe);
         }
 
+        async function pingdomFetcher(config, context, timeframe, fetcher) {
+            return fetcher('pingdom', config, context, timeframe);
+        } 
+
         async function run(config, context, timeframe, fetcher) {
-            const gremlinQuery = 'g.V().has("sourceName", sourceName).count()';
-            const bindings = { sourceName: config.vars.sourceName };
-            const data = await graphFetcher(config, context, timeframe, fetcher, gremlinQuery, bindings);
-            return data;
+            let data;
+            if (!context.sourceId) {
+                const gremlinQuery = 'g.V().has("sourceName", sourceName).limit(10)';
+                const bindings = { sourceName: config.vars.sourceName };
+                data = await graphFetcher(config, context, timeframe, fetcher, gremlinQuery, bindings);
+                if (sourceName !== 'Pingdom') {
+                        return data;
+                }
+            } else {
+                data = [context];
+            }
+            return Promise.all(data.map(async ctx => {
+                const pingdomConfig = {
+                        id: ctx.sourceId[0];
+                }
+                return pingdomFetcher(pingdomConfig, ctx, timeframe, fetcher);
+            }));
         }
