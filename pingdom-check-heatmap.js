7c78c9147fa0832b07c7106fa45b9a6cf76e4ca2
Richard Jones <wigsaparelli@gmail.com>
2021-05-21 14:03:51 +0100

    Update pingdom-check-heatmap.js

--- pingdom-check-heatmap.js
+++ pingdom-check-heatmap.js
@@ -1,28 +1,32 @@
-        async function graphFetcher(config, context, timeframe, fetcher, gremlinQuery, bindings) {
-            return fetcher('graph-custom', { gremlinQuery, bindings }, context, timeframe);
+async function graphFetcher(graphConfig, context, timeframe, fetcher) {
+        return fetcher('graph-custom', graphConfig, context, timeframe);
+}
+
+async function pingdomFetcher(pingdomConfig, context, timeframe, fetcher) {
+        return fetcher('pingdom', pingdomConfig, context, timeframe);
+} 
+
+async function run(config, context, timeframe, fetcher) {
+        let data;
+
+        if (context.sourceId && context.sourceName) {
+                const sourceName = context.sourceName[0];
+                if (sourceName !== 'Pingdom') {
+                        return 'Object is not a Pingdom check';
+                }
+                data = [context];
+        } else {
+                const limit = config.vars?.limit || 10;
+                const gremlinQuery = 'g.V().has("sourceName", sourceName).limit(limit).valueMap(true)';
+                const bindings = { sourceName: 'Pingdom', limit };
+                const graphConfig = { gremlinQuery, bindings };
+                data = await graphFetcher(graphConfig, context, timeframe, fetcher);
         }
 
-        async function pingdomFetcher(config, context, timeframe, fetcher) {
-            return fetcher('pingdom', config, context, timeframe);
-        } 
-
-        async function run(config, context, timeframe, fetcher) {
-            let data;
-            if (!context.sourceId) {
-                const sourceName = config.vars.sourceName;
-                const gremlinQuery = 'g.V().has("sourceName", sourceName).limit(10).valueMap(true)';
-                const bindings = { sourceName };
-                data = await graphFetcher(config, context, timeframe, fetcher, gremlinQuery, bindings);
-                if (sourceName !== 'Pingdom') {
-                        return data;
-                }
-            } else {
-                data = [context];
-            }
-            return Promise.all(data.map(async ctx => {
+        return Promise.all(data.map(async ctx => {
                 const pingdomConfig = {
                         id: ctx.sourceId[0]
                 }
                 return pingdomFetcher(pingdomConfig, ctx, timeframe, fetcher);
-            }));
-        }
+        }));
+}
