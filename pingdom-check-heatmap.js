306c78f124bdd6af21fec38f68fd7e501c794bdf
Richard Jones <wigsaparelli@gmail.com>
2021-05-27 16:58:13 +0100

    Update pingdom-check-heatmap.js

--- pingdom-check-heatmap.js
+++ pingdom-check-heatmap.js
@@ -1,12 +1,12 @@
-async function run(config, context, timeframe, fetcher) {
+async function run(config, context, timeframe, api) {
     try {
         /* Determine the context (Pingdom graph nodes) for the Pingdom data request */
-        const nodes = await getGraphNodes(config, context, timeframe, fetcher);
+        const nodes = await getGraphNodes(config, context, timeframe, api);
 
         /* Get the Pingdom timeseries data for the Pingdom graph nodes */
         const data = await Promise.all(nodes.map(async node => {
             const pingdomConfig = { id: node.sourceId[0] };
-            return fetcher('pingdom', pingdomConfig, node, timeframe);
+            return api.fetch('pingdom', pingdomConfig, node, timeframe);
         }));
 
         /* Transform the Pingdom timeseries data to that required by a Heatmap */
@@ -30,7 +30,7 @@ async function run(config, context, timeframe, fetcher) {
     }
 }
 
-async function getGraphNodes(config, context, timeframe, fetcher) {
+async function getGraphNodes(config, context, timeframe, api) {
     if (context.sourceId && context.sourceName) {
         if (context.sourceName[0] !== 'Pingdom') {
             throw new Error('Object is not a Pingdom check');
@@ -41,6 +41,6 @@ async function getGraphNodes(config, context, timeframe, fetcher) {
         const gremlinQuery = 'g.V().has("sourceName", sourceName).limit(limit).valueMap(true)';
         const bindings = { sourceName: 'Pingdom', limit };
         const graphConfig = { gremlinQuery, bindings };
-        return fetcher('graph-custom', graphConfig, context, timeframe);
+        return api.fetch('graph-custom', graphConfig, context, timeframe);
     }
 }
